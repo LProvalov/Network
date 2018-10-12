e29367df55708fb440d2e653da1e9aea3b86b0d8
Lev Provalov <lev.provalov@harman.com>
2018-10-12 16:21:42 +0300

    Upload projects, change ignore file

---
+++ SocketChat/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Concurrent;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int port = 8005;
+        static void Main(string[] args)
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, port);
+
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listenSocket.Bind(ipPoint);
+                listenSocket.Listen(3);
+                ConcurrentDictionary<int, Socket> sockets = new ConcurrentDictionary<int, Socket>();
+
+                var t = listenSocket.AcceptAsync();
+                t.ContinueWith(socket => {
+                    sockets.TryAdd(socket.Id, socket);
+                });
+                Socket handler = listenSocket.Accept();
+                byte[] receivedBuffer = new byte[1024];
+                ArraySegment<byte> buffer = new ArraySegment<byte>(receivedBuffer);
+
+                while (true)
+                {
+                    handler.ReceiveAsync(buffer, SocketFlags.Broadcast).ContinueWith(t => {
+                        Console.WriteLine("{0}", buffer);
+                    });
+                    
+
+                    handler.Receive(receivedBuffer);
+                    string receivedMsg = ASCIIEncoding.ASCII.GetString(receivedBuffer);
+                    Console.WriteLine("{0}", receivedMsg);
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ SocketChatClient/SocketChatClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            EndPoint serverEp = new IPEndPoint(address, 8005);
+            socket.Connect(serverEp);
+            while (true) {
+                string line = Console.ReadLine();
+                byte[] buffer = ASCIIEncoding.ASCII.GetBytes(line);
+                socket.Send(buffer);
+            }
+            
+        }
+    }
+}

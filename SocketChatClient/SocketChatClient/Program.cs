9f287be9d764f6e11cbdf3625d7064edd73f5423
Lev Provalov <lev.provalov@harman.com>
2018-10-15 17:10:56 +0300

    Added methods for send message from client to server

--- SocketChatClient/SocketChatClient/Program.cs
+++ SocketChatClient/SocketChatClient/Program.cs
@@ -14,12 +14,21 @@ namespace SocketChatClient
             IPAddress address = IPAddress.Parse("127.0.0.1");
             EndPoint serverEp = new IPEndPoint(address, 8005);
             socket.Connect(serverEp);
+            Console.Write("Enter client name: ");
+            string clientName = Console.ReadLine();
+            sendMessage(socket, clientName);
+
             while (true) {
                 string line = Console.ReadLine();
-                byte[] buffer = ASCIIEncoding.ASCII.GetBytes(line);
-                socket.Send(buffer);
-            }
-            
+                sendMessage(socket, line);
+            }            
+        }
+
+        static void sendMessage(Socket socket, string message)
+        {
+            byte[] messageBuffer = ASCIIEncoding.ASCII.GetBytes(message);
+            socket.Send(BitConverter.GetBytes(messageBuffer.Length));
+            socket.Send(messageBuffer);
         }
     }
 }
